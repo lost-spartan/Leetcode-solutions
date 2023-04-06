8f8c44b17015bdbbb397088037171242fa221793
Kushagra Mani <80683602+lost-spartan@users.noreply.github.com>
2023-04-07 00:17:25 +0530

    234. Palindrome Linked List
    Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
    
    Input: head = [1,2,2,1]
    Output: true

--- Palindrome Linked List.java
+++ Palindrome Linked List.java
@@ -1,47 +1,33 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
-  public boolean isPalindrome(ListNode head) {
-    ListNode slow = head;
-    ListNode fast = head;
-
-    while (fast != null && fast.next != null) {
-      slow = slow.next;
-      fast = fast.next.next;
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if(fast != null){
+            slow = slow.next;
+        }
+        ListNode rev = reverse(slow);
+        while(rev != null){
+            if(rev.val != head.val)
+                return false;
+            else{
+                rev = rev.next;
+                head = head.next;
+            }
+        }
+        return true;
     }
-
-    if (fast != null)
-      slow = slow.next;
-    slow = reverseList(slow);
-
-    while (slow != null) {
-      if (slow.val != head.val)
-        return false;
-      slow = slow.next;
-      head = head.next;
+    public ListNode reverse(ListNode head){
+        ListNode prev = null;
+        while(head != null){
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        return prev;
     }
-
-    return true;
-  }
-
-  private ListNode reverseList(ListNode head) {
-    ListNode prev = null;
-
-    while (head != null) {
-      ListNode next = head.next;
-      head.next = prev;
-      prev = head;
-      head = next;
-    }
-
-    return prev;
-  }
 }
